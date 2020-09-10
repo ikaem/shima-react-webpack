11555f56c37cf39c878f56e70d7ffb7725ec7532
Karlo Marinović <marinovic.karlo@gmail.com>
2020-09-10 11:41:16 +0200

    finished webpack setup

---
+++ src/index.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+ki
+
+const App = () => {
+  return <div>Hello React and TypeScript! { boom }</div>;
+};
+
+ReactDOM.render(<App />, document.getElementById("root"));
