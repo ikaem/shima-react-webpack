1865606456132fb84d5a7fd1ef5c608cf568b60a
Karlo Marinović <marinovic.karlo@gmail.com>
2020-09-22 10:48:22 +0200

    prior to ca deploy

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var PORT = process.env.PORT || 8500;
 var app = express();
 // NOW WE set to a directory from which we will server static files. in this case, it is build folder
 app.use(express.static(path.join(__dirname, "build"), { index: false }));
-app.use(express.static(path.join(__dirname, "public"), { index: false }));
+// app.use(express.static(path.join(__dirname, "public"), { index: false }));
 // send user to index.html despite the url
 app.get("*", function (req, res) {
     res.sendFile(path.resolve(__dirname, "./public/index.html"));
